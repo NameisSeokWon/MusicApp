9f191f129311aa481ed172d2e9a7e26c06910108
CSS <ccss1209@naver.com>
2024-04-05 22:21:46 +0900

    first music

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <h1>Presented by Weavus</h1>
+            <div>株式会社WEAVUS 〒160-0022 東京都新宿1-19-10 サンモールクレスト205号</div>
+        </React.Fragment>
+    );
+}
+
+export default About;
